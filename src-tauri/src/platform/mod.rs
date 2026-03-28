f0bbcb5a58cf41ae9471dbf3bbd04ff1b2fc1403
Emilio Roma <emilioroma2212@gmail.com>
2026-03-27 22:05:48 -0600

    fix: support for user verification on non-Windows platforms

--- src-tauri/src/platform/mod.rs
+++ src-tauri/src/platform/mod.rs
@@ -1,8 +1,15 @@
 #[cfg(windows)]
 mod windows;
 
+#[cfg(not(windows))]
+mod non_windows;
 
 #[cfg(windows)]
 pub async fn request_user_verification(window: &tauri::Window) -> Result<(), String> {
     windows::request_user_verification(window).await
+}
+
+#[cfg(not(windows))]
+pub async fn request_user_verification(window: &tauri::Window) -> Result<(), String> {
+    non_windows::request_user_verification(window).await
 }
